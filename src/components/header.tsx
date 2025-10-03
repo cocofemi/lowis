584b0991977acdd4846174341fcef54bc92cf782
femi <cocofemi@gmail.com>
2025-10-03 16:34:34 +0100

    changes

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -6,7 +6,7 @@ export default function Header() {
   return (
     <header className="flex h-16 shrink-0 items-center justify-between gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
       <div className="flex items-center gap-2 px-4">
-        {/* <SidebarTrigger /> */}
+        <SidebarTrigger />
         {/* <SidebarTrigger className="" /> */}
         <Separator orientation="vertical" className="h-4" />
         <p className="text-bold text-white">Lowis </p>
